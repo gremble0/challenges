084118c6cc2b94e02f34bbfdedead0e97f687648
gremble0 <hermanstornes@gmail.com>
2024-01-16 15:59:21 +0100

    Python solution for longestPalindrome

---
+++ medium/longestPalindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        longest = ""
+        iter = ""
+
+        for i in range(0, len(s)):
+            for j in range(i, len(s)):
+                iter += s[j]
+
+                if iter == iter[::-1] and len(iter) >= len(longest):
+                    longest = iter
+
+            iter = ""
+
+        return longest
+
+a = Solution()
+print(a.longestPalindrome("a"))
